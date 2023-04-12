b9983781a90a2d05a5617fea97628ab9c43b0ccd
AndreaCollura <andrea.collur@gmail.com>
2023-04-12 16:10:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+:<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<script>
+import AppTitle from './components/AppHeader.vue';
+  export default {
+    name: 'App',
+    components: {
+      AppTitle
+    
+    }
+    
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    name:'AppTitle',
+    data(){
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
